9dbe47f385e519085a3c8f4198f7393718ed47cf
Kiramman <kiramman89@gmail.com>
2023-11-06 19:41:15 +0700

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+import java.util.SplittableRandom;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+}
